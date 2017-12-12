001eb3553fe9ab14a8e5514653717bbbc0fe4fcd
Sigrun Sunna <sigrunsg16@ru.is>
2017-12-12 16:24:39 +0000

    add topping valmöguleiki færður í management

--- mainPizza/src/ui/ManagementUI.cpp
+++ mainPizza/src/ui/ManagementUI.cpp
@@ -1,20 +1,50 @@
 #include "ManagementUI.h"
 #include "PizzaRepository.h"
+#include "ToppingRepository.h"
 
 void ManagementUI::Management()
 {
 
+    ToppingRepository toppingRepo;
     char inp = '\0';
     while(inp != 'q')
     {
         cout << "Hello Manager, would you like to:" << endl;
-        cout << "v: view orders, q: quit?: ";
+        cout << "t: add toppings" << endl;
+        cout << "v: view orders" << endl;
+        cout << "q: quit " << endl;
+        cout << ": ";
         cin >> inp;
         if (inp == 'v')
         {
 
             PizzaRepository pizzaRepo;
             pizzaRepo.retrieveALL();
+        }
+        if(inp == 't')
+        {
+            vector<Toppings> toppings = toppingRepo.retrieveAllToppings();
+            cout << "These are the current toppings in the system: " << endl;
+            for(unsigned int i = 0; i < toppings.size(); i++)
+            {
+                cout << "[" << i + 1 << "] " << toppings[i] << endl;
+            }
+            cout << endl;
+
+            char selection = 'y';
+            Toppings t;
+            while(selection == 'y')
+            {
+                cout << "Add another topping (y)?: ";
+                cin >> selection;
+                if(selection == 'y'){
+                    cin >> t;
+                    toppings.push_back(t);
+                }
+            }
+            toppingRepo.storeAllToppings(toppings);
+
+
         }
     }
 }
--- mainPizza/src/ui/SalesUI.cpp
+++ mainPizza/src/ui/SalesUI.cpp
@@ -14,7 +14,6 @@ void SalesUI::Sales()
         cout << "m: make pizza" << endl;
         cout << "r: read pizza" << endl;
         cout << "q: quit" << endl;
-        cout << "t: add toppings" << endl;
         cin >> selection;
 
         if(selection == 'm')
@@ -52,31 +51,7 @@ void SalesUI::Sales()
 
             cout << endl;
         }
-        if(selection == 't')
-        {
-            vector<Toppings> toppings = toppingRepo.retrieveAllToppings();
-            cout << "These are the current toppings in the system: " << endl;
-            for(unsigned int i = 0; i < toppings.size(); i++)
-            {
-                cout << "[" << i + 1 << "] " << toppings[i] << endl;
-            }
-            cout << endl;
 
-            char selection = 'y';
-            Toppings t;
-            while(selection == 'y')
-            {
-                cout << "Add another topping (y)?: ";
-                cin >> selection;
-                if(selection == 'y'){
-                    cin >> t;
-                    toppings.push_back(t);
-                }
-            }
-            toppingRepo.storeAllToppings(toppings);
-
-
-        }
         if(selection != 'm' && selection != 'r' && selection != 'q' && selection != 't')
         {
             cout << endl;
